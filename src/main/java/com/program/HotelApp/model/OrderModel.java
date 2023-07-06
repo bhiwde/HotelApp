99a76e79550f3e020f5c7190f08496ea7f9631ac
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-07-07 00:26:00 +0530

    [ADDED]:[Nirmla] create interface for customberservice

---
+++ src/main/java/com/program/HotelApp/model/OrderModel.java
@@ -0,0 +1,36 @@
+package com.program.HotelApp.model;
+
+public class OrderModel {
+	private int oid;
+	private int cid;
+	private String date;
+	private int amount;
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+
+
+}
---
+++ src/main/java/com/program/HotelApp/service/ICustomberService.java
@@ -0,0 +1,14 @@
+package com.program.HotelApp.service;
+
+import java.util.ArrayList;
+
+import com.program.HotelApp.model.CustomerModel;
+
+public interface ICustomberService {
+	
+ public String addcustomber(CustomerModel custombermodel);
+ public ArrayList getallcustomber();
+	 
+ 
+
+}
